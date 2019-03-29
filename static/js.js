27478a179f66db9bb4fdcfe4d5cb719fdb01e4c7
leilei.yang <13764643510@163.com>
2019-03-29 18:00:04 +0800

    Update js.js

--- static/js.js
+++ static/js.js
@@ -209,9 +209,6 @@ document.getElementById('make_sure_send').onclick = function () {
 			alertMsg("1234566777");
             if (ajax.readyState == 4) {
                 if (ajax.status >= 200 && ajax.status < 300 || ajax.status == 304) {
-                    console.log("上传成功");
-                    var obj = JSON.parse(ajax.responseText);
-                    // console.log(obj.fileURL);
                     alertMsg("上传成功");
                 }
 				else
